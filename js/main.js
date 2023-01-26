e81e681a7da78bd349f85ecca3ab0f68e1d48426
Joshua Mclean <86990609+JoshuaMc1@users.noreply.github.com>
2023-01-25 23:47:41 -0600

    Carrito de compras completado

--- js/main.js
+++ js/main.js
@@ -56,6 +56,88 @@ $(document).ready(function () {
         e.preventDefault();
     });
 
+    search.blur(function (e) {
+        resultSearch.addClass("d-none");
+        e.preventDefault();
+    });
+
+    searchInputMobile.blur(function (e) {
+        resultSearch.addClass("d-none");
+        e.preventDefault();
+    });
+
+    $.fn.inputStyle = function (options) {
+        var settings = $.extend({
+            value: 0,
+            step: undefined,
+            min: undefined,
+            max: undefined
+        }, options);
+
+        return this.each(function () {
+            var input = $(this);
+            var container = document.createElement('div');
+            var btnAdd = document.createElement('div');
+            var btnRem = document.createElement('div');
+            var min = settings.min || input.attr('min');
+            var max = settings.max || input.attr('max');
+            var value = settings.value || parseFloat(input.val());
+
+            container.className = 'input-number-qty';
+            btnAdd.className = (max && value >= max) ? 'qty-btn qty-add disabled' : 'qty-btn qty-add';
+            btnAdd.innerHTML = '+';
+            btnRem.className = (min && value <= min) ? 'qty-btn qty-rem disabled' : 'qty-btn qty-rem';
+            btnRem.innerHTML = '-';
+            input.wrap(container);
+            input.closest('.input-number-qty').prepend(btnRem).append(btnAdd);
+
+            $(document).off('click', '.qty-btn').on('click', '.qty-btn', function (e) {
+                var input = $(this).siblings('input');
+                var sibBtn = $(this).siblings('.qty-btn');
+                var step = settings.step || parseFloat(input.attr('step'));
+                var min = settings.min || input.attr('min');
+                var max = settings.max || input.attr('max');
+                var oldValue = parseFloat(input.val());
+                var newVal;
+
+                if ($(this).hasClass('qty-add')) {
+                    newVal = (oldValue >= max) ? oldValue : oldValue + step;
+                    newVal = (newVal > max) ? max : newVal;
+
+                    if (newVal === max) {
+                        $(this).addClass('disabled');
+                    }
+                    sibBtn.removeClass('disabled');
+                } else {
+                    newVal = (oldValue <= min) ? oldValue : oldValue - step;
+                    newVal = (newVal < min) ? min : newVal;
+
+                    if (newVal === min) {
+                        $(this).addClass('disabled');
+                    }
+                    sibBtn.removeClass('disabled');
+                }
+                input.val(newVal).trigger('change');
+            });
+
+            input.on('change', function () {
+                var val = parseFloat(input.val());
+                var min = settings.min || input.attr('min');
+                var max = settings.max || input.attr('max');
+
+                if (val > max) {
+                    input.val(max);
+                }
+
+                if (val < min) {
+                    input.val(min);
+                }
+            });
+        });
+    };
+
+    $('.input-number').inputStyle();
+
     function showContainerSearchResult(e) {
         if (e.target.value.length >= 2) {
             resultSearch.removeClass("d-none");
